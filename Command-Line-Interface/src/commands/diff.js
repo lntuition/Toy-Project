c2f8f13d5a9a5ce9a697ecf5ebd9b04a5a47cd55
lntuition <ekffu200098@gmail.com>
2019-09-01 23:35:06 +0900

    Finish oclif project

--- Command-Line-Interface/src/commands/diff.js
+++ Command-Line-Interface/src/commands/diff.js
@@ -1,10 +1,20 @@
 const { Command } = require('@oclif/command');
+const moment = require('moment');
 
 class DiffCommand extends Command {
   async run() {
     const { args } = this.parse(DiffCommand);
+    let diffMinute = moment(args.endTime, 'HH:mm').diff(moment(args.startTime, 'HH:mm'), 'minutes');
 
-    this.log(args)
+    let sign = '';
+    if (diffMinute < 0) {
+      diffMinute = -diffMinute;
+      sign = '-';
+    }
+    let hour = String(parseInt(diffMinute / 60)).padStart(2, '0');
+    let minute = String(diffMinute % 60).padStart(2, '0');
+
+    this.log(sign + hour + ":" + minute);
   }
 }
 
